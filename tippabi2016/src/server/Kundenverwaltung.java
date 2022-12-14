e020a3d4f59699b6dd8542e8f8f254831527a5fa
Q2.02Trell <Q2.02Trell@R241C09X.SGNW.local>
2022-12-14 11:21:30 +0100

    Merge origin/master
    Conflicts:
    	tippabi2016/src/server/Kundenverwaltung.java

--- tippabi2016/src/server/Kundenverwaltung.java
+++ tippabi2016/src/server/Kundenverwaltung.java
@@ -11,23 +11,75 @@ import abiturklassen.listenklassen.List;
  * @author q2.02Schaaf
  */
 public class Kundenverwaltung {
+
     List<User> userList = new List<>();
 
     public int anmelden(String benutzername, String passwort, String ip, int port) {
         userList.toFirst();
-        while(userList.hasAccess()){
-            if(benutzername == userList.getContent().getBenutzername()){
-                if(passwort == userList.getContent().getPasswort()){
-                    if(userList.getContent().getIP() == null && userList.getContent().getPort() == 0) return 1;
-                    else return 2;}
-                else return 3;}                       
-            else return 4;}     
-        return 5;    
-}
-    
-    public void erstelleUser(String benutzername, String passwort){
+        while (userList.hasAccess()) {
+            if (benutzername == userList.getContent().getBenutzername()) {
+                if (passwort == userList.getContent().getPasswort()) {
+                    if (userList.getContent().getIP() == null && userList.getContent().getPort() == 0) {
+                        userList.getContent().setIP(ip);
+                        userList.getContent().setPort(port);
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                } else {
+                    return 2;
+                }
+            }
+            userList.next();
+        }
+        return 3;
+    }
+
+    public void erstelleUser(String benutzername, String passwort) {
         userList.append(new User(benutzername, passwort));
     }
+
+    public int getUserID(String ip, int port) {
+        userList.toFirst();
+        while (userList.hasAccess()) {
+            if (ip == userList.getContent().getIP() && port == userList.getContent().getPort()) {
+                return userList.getContent().getUserID();
+            }
+            userList.next();
+        }
+        return -1;
+    }
+
+    public void setzeTipp(int userID, int spielID, int tor1, int tor2) {
+        userList.toFirst();
+        while (userList.hasAccess()) {
+            if (userID == userList.getContent().getUserID()) {
+                userList.getContent().setSpieleTipps(spielID, tor1, tor2);
+            }
+        }
+        userList.next();
+    }
+
+    public int[] getTipp(int userID, int spielID) {
+        userList.toFirst();
+        while (userList.hasAccess()) {
+            if (userID == userList.getContent().getUserID()) {
+                return userList.getContent().getSpieleTipps()[spielID];
+            }
+            userList.next();
+        }
+
+        return null;
+    }
+
+    public void abmelden(int userID) {
+        userList.toFirst();
+        while (userList.hasAccess()) {
+            if (userID == userList.getContent().getUserID()) {
+                userList.getContent().setIP(null);
+                userList.getContent().setPort(0);
+            }
+            userList.next();
+        }
+    }
 }
-
-
--- tippabi2016/src/server/User.java
+++ tippabi2016/src/server/User.java
@@ -1,30 +1,33 @@
 package server;
 
-
 import abiturklassen.listenklassen.List;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Q2.02Trell
  */
 public class User {
+
     String benutzername;
     String passwort;
     static int nextUserID = 0;
     int userID;
     String IP;
     int port = 0;
-    List<Integer> spieleTipps = new List<>();
+    int[][] spieleTipps = new int[51][2];
 
     public User(String benutzername, String passwort) {
         this.benutzername = benutzername;
         this.passwort = passwort;
         userID = nextUserID++;
+        for (int i = 0; i < 51; i++) {
+            spieleTipps[i][0] = -1;
+            spieleTipps[i][1] = -1;
+        }
     }
 
     public String getBenutzername() {
@@ -75,12 +78,12 @@ public class User {
         this.port = port;
     }
 
-    public List<Integer> getSpieleTipps() {
+    public int[][] getSpieleTipps() {
         return spieleTipps;
     }
 
-    public void setSpieleTipps(List<Integer> spieleTipps) {
-        this.spieleTipps = spieleTipps;
+    public void setSpieleTipps(int spielID, int tor1, int tor2) {
+        spieleTipps[spielID][0] = tor1;
+        spieleTipps[spielID][1] = tor2;
     }
-
 }
