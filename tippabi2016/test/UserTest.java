f446d2584f9a5a7bfb5c0c54547582d1ecddfb13
Pardella <Pardella@BPx001>
2022-12-18 11:42:22 +0100

    Kleiner Test, ob die ID beim User funktioniert.

---
+++ tippabi2016/test/UserTest.java
@@ -0,0 +1,27 @@
+
+import server.User;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Pardella
+ */
+public class UserTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        User u1 = new User("a","1");
+        User u2 = new User("b","2");
+        System.out.println(u1.getUserID()+" "+u2.getUserID());
+    }
+    
+}
